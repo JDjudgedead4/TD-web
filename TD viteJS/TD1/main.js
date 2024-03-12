348f84bde634c9e1bf9dff9739e3a7e8f898d22e
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-03-12 11:52:51 +0100

    map finis

--- TD viteJS/TD1/main.js
+++ TD viteJS/TD1/main.js
@@ -2,29 +2,32 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import Cookies from 'js-cookie';
+import L from 'leaflet';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+
 `
 const couleurInput = document.getElementById("couleur");
+const latitude = 48.856614;
+const longitude = 2.348803;
+const zoom = 13;
+const map = L.map('map').setView([latitude, longitude], zoom);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+//map.invalidateSize();
 
+
+document.body.style.backgroundColor = Cookies.get('couleurChoisie')
 couleurInput.addEventListener("change", () => {
     document.body.style.backgroundColor = couleurInput.value;
-
+    Cookies.set('couleurChoisie', couleurInput.value);
     console.log("La nouvelle couleur est : " + couleurInput.value);
 });
+
+
 setupCounter(document.querySelector('#counter'))
